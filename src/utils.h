c7139d69c54ce91beadd8b5bb01c723a08510028
anore <66754841+codelumen@users.noreply.github.com>
2022-09-17 18:06:28 +0300

    migrated & refactored (see desc)
    migrated to utils.tcc (because of templates)
    implemented LoadingIndicator
    etc

--- src/utils.h
+++ src/utils.h
@@ -1,22 +1,41 @@
 #pragma once
 
+#ifndef UTILS_H
+#define UTILS_H
+
 #include <vector>
+#include <string>
+#include <chrono>
 
 
 namespace utils {
-	/*
-	 * Array(vector) functions.
-	 */
+	class LoadingIndicator {
+		public:
+			static std::vector<std::string> braileSymbols;
+
+			int index;
+
+			LoadingIndicator();
+
+			std::string render();
+			std::string next();
+			std::string purge();
+	};
 
 	template <typename T>
 	T average(std::vector<T> v);
 
-	template <typename T>
-	T median(std::vector<T> v);
+	unsigned long long median(std::vector<unsigned long long> v);
 
 	template <typename T>
 	T max(std::vector<T> v);
 
 	template <typename T>
 	T min(std::vector<T> v);
+
+	std::string formatTime(std::chrono::nanoseconds t);
 }
+
+#include "utils.tcc"
+
+#endif
